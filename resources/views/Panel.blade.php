1a6a13328751b45be80fbd2366f5a9cc1bbd0253
Unknown <bernar_1996@hotmail.com>
2017-11-13 23:12:09 -0700

    menu del usuario terminado

--- resources/views/Panel.blade.php
+++ resources/views/Panel.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-8 col-md-offset-2">
             <div class="">
                     @include('layouts.NavBarUsuario')
+                    <img src="https://icon-icons.com/icons2/317/PNG/512/user-male-icon_34332.png" alt="perfil" class="img-rounded" alt="Cinque Terre" width="100" height="100" style="border: 2px solid silver;">
                 </div>
             </div>
         </div>
--- resources/views/layouts/NavBarUsuario.blade.php
+++ resources/views/layouts/NavBarUsuario.blade.php
@@ -1,10 +1,23 @@
-
+<style>
+  .navbar-brand {
+    float: left !important;
+    padding: 9.5px 17px !important;
+    font-size: 18px !important;
+    line-height: 21px !important;
+    height: 40px !important;
+}
+</style>
 
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-nav" >
-
-
+        <a class="navbar-brand" href="#">Mi panel</a>
+        <a class="navbar-brand" href="#">Cambiar contraseña</a>
+        <a class="navbar-brand" href="#">Solicitar residencia</a>
+        <a class="navbar-brand" href="#">Notificaciones</a>
+        <a class="navbar-brand" href="#">Instrucciones</a>
+        <a class="navbar-brand" href="#">Avances</a>
+        <a class="navbar-brand" href="#">Salir</a>
     </div>
   </div>
 </nav>
