e20e640223d21a0bbf839a1db9a343d91c6650ac
Unknown <bernar_1996@hotmail.com>
2017-11-30 06:53:40 -0700

    validacion planes

--- resources/views/avances.blade.php
+++ resources/views/avances.blade.php
@@ -2,14 +2,20 @@
 @section('content')
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="">
+                <div class="" text-align="center">
                     @include('layouts.NavBarJefeDivision')
-        
+        			@if ($alumnos->ESTADO==1 )
+        			<div class="alert alert-success alert-block" style="height:200px;text-align:center;padding-top: 75px">
+                                    
+                                          <strong x">SU SOLICITUD SE ENCUENTRA EN PROCESO </strong>
+                                  </div>
+        		
+        			@endif
                 </div>
             </div>
         </div>
 
-ESTA PAGINA SE ENCUENTRA EN DESARROLLO(avances)
+
 
 
 @endsection
