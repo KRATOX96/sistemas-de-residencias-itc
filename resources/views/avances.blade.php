2fd46c5f9e86ce578ff82010518bbeded3568798
Unknown <bernar_1996@hotmail.com>
2017-12-02 19:53:55 -0700

    condicionales de 2 estados faltantes del alumno

--- resources/views/avances.blade.php
+++ resources/views/avances.blade.php
@@ -4,13 +4,22 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="" text-align="center">
                     @include('layouts.NavBarJefeDivision')
-        			@if ($alumnos->ESTADO==1 )
+        			@if ($alumnos->ESTADO ==1 || $alumnos->ESTADO ==2 || $alumnos->ESTADO ==4 || $alumnos->ESTADO ==5   )
         			<div class="alert alert-success alert-block" style="height:200px;text-align:center;padding-top: 75px">
                                     
                                           <strong x">SU SOLICITUD SE ENCUENTRA EN PROCESO </strong>
                                   </div>
         		
         			@endif
+
+                    @if ($alumnos->ESTADO ==3)
+                        
+                    @endif
+
+                    @if ($alumnos->ESTADO ==6)
+                        
+                    @endif
+
                 </div>
             </div>
         </div>
